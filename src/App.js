c910fa98cd7b4546cf587062464fd8603a214b2f
Julie "Jous" Paillusseau <jous@akiros.io>
2024-02-25 10:48:58 +0100

    Customize the app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Confetti from "./Confetti";
+import logo from './logo.svg';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
+      <Confetti />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <h1 style={{ marginBottom: "0px" }}>Congratulations !!!</h1>
+        <p style={{ marginTop: "10px", marginBottom: "50px" }}>
+          Your container is running 🙌🏻
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
-}
+};
 
 export default App;
